5af6299e5e51193515ef5602f77f45406c04135e
catalin <catalin.casuneanu@gmail.com>
2021-01-27 23:53:45 +0200

    Routing Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-     
-    </div>
-  );
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Form from './pages/Form'
+import Logout from './pages/Logout'
+
+export default function App(){
+  localStorage.setItem('is_logged_in', false);
+  return(
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Router path="/form">
+         <Form/>
+        </Router>
+        <Router path="/logout">
+          <Logout/>
+        </Router>
+      </Switch>
+    </Router>
+  )
 }
-
-export default App;
